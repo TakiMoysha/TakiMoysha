a0a43cbb1a5aa5aeb8f05ea3ab4bbd5ce93eb295
Mikhail Popov <36836047+TakiMoysha@users.noreply.github.com>
2024-03-13 14:18:34 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+yay -S ripgrep neovim kate clipit alacritty rofi
+yay -S noto-fonts-cjk noto-fonts-emoji noto-fonts
+# yay -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family 

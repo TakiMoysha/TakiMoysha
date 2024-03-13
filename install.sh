eec8e77b170ebeaddf7364cbdfcaa3f9c9371f08
Mikhail Popov <36836047+TakiMoysha@users.noreply.github.com>
2024-03-13 22:27:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 yay -S ripgrep neovim kate clipit alacritty rofi
 yay -S noto-fonts-cjk noto-fonts-emoji noto-fonts
 # yay -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-inconsolata ttf-ubuntu-font-family 
+
+npm install -g \
+  typescript-language-server \
+  @vue/typescript-plugin \
+  typescript
